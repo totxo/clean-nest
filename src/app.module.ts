cf3259eb8b93aea54aa7a5aac41b810674cd9b68
Totxo <tomaschorroarin@gmail.com>
2020-11-01 22:08:48 -0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { ApplicationModule } from './application/application.module';
+import { DomainModule } from './domain/domain.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+
+@Module({
+  imports: [
+    ApplicationModule,
+    DomainModule,
+    InfrastructureModule
+  ]
+})
+export class AppModule {}
